e5ff84a18e8148e89e42930962470b5773672e51
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-05-31 05:06:51 +0500

    added books from backend

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import Banner from "./components/Banner";
+import BookList from "./components/BookList";
+
+
+const page = () => {
+  return <div>
+    <h1 className="text-3xl font-bold underline">
+      <Banner/>
+      <BookList/>?
+    </h1>
+  </div>;
+};
+
+export default page;
---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="min-h-[800px]  mx-auto max-w-7xl px-5 pb-10 mb-6">
+      Loading please wait...
+    </div>
+  );
+};
+
+export default Loading;
